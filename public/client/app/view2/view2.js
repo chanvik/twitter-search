dce475e188db89c391388c905f92ddfe4346f424
chanvik <chanvi.kotak@gmail.com>
2015-12-06 19:16:32 -0500

    add comment and view comments  completed

--- public/client/app/view2/view2.js
+++ public/client/app/view2/view2.js
@@ -11,16 +11,34 @@ angular.module('myApp.view2', ['ngRoute'])
 
 .controller('TweetCommentCtrl', ["$scope", "$routeParams", "$http", function($scope, $routeParams , $http ) {
 	$scope.id = $routeParams.id;
+	$scope.tweet = {
+		comment : ""
+	};
 	(function (){
-		$http.post('/api/v2/twitter/findbyid',{id:$scope.id}).success(function(response){
-			var tweets = []
+		$http.post('/api/v2/twitter/findbyid',{id:$scope.id}).success(formatData);
+	})()
+
+	$scope.save=function(){
+		var tweet = $scope.stringlist[0];
+		var comments = tweet.comment && tweet.comment.length>0 ? tweet.comment : [];
+		comments.push($scope.tweet.comment);
+		$scope.tweet.comment = "";
+		$http.post('/api/v2/twitter/savecomment',{id:$scope.id, comments:comments}).success(formatData);
+	}
+
+	function formatData(response){
+		var tweets = []
 			response.forEach(function(data){
 				var tweet = data;
+				var comment = [];
+				tweet.comment.forEach(function(data){
+					comment.push({text:data});
+				})
+				tweet.comment = comment;
 				var dateTime = new Date(tweet.createdAt)
 				tweet.createdAt =  dateTime.getMonth()  + "/" + dateTime.getDay() + "/" + dateTime.getFullYear();
 				tweets.push(tweet) 
 			})
-			$scope.stringlist = tweets;
-		});
-	})()
+		$scope.stringlist = tweets;
+	}
 }]);
