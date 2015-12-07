5187b36d75fe2d8f2ba8c724148ae345a99905d6
chanvik <chanvi.kotak@gmail.com>
2015-12-06 20:09:31 -0500

    comment bug fixed

--- public/client/app/view2/view2.js
+++ public/client/app/view2/view2.js
@@ -14,12 +14,13 @@ angular.module('myApp.view2', ['ngRoute'])
 	$scope.tweet = {
 		comment : ""
 	};
+	$scope.tweets = {}
 	(function (){
 		$http.post('/api/v2/twitter/findbyid',{id:$scope.id}).success(formatData);
 	})()
 
 	$scope.save=function(){
-		var tweet = $scope.stringlist[0];
+		var tweet = $scope.tweets[0];
 		var comments = tweet.comment && tweet.comment.length>0 ? tweet.comment : [];
 		comments.push($scope.tweet.comment);
 		$scope.tweet.comment = "";
@@ -28,19 +29,20 @@ angular.module('myApp.view2', ['ngRoute'])
 
 	function formatData(response){
 		var tweets = []
-			response.forEach(function(data){
-				var tweet = data;
-				var comment = [];
-				if(tweet.comment){
-					tweet.comment.forEach(function(data){
-						comment.push({text:data});
-					})
-					tweet.comment = comment;
-				}
-				var dateTime = new Date(tweet.createdAt)
-				tweet.createdAt =  dateTime.getMonth()  + "/" + dateTime.getDay() + "/" + dateTime.getFullYear();
-				tweets.push(tweet) 
-			})
+		$scope.tweets = response;	
+		response.forEach(function(data){
+			var tweet = data;
+			var comment = [];
+			if(tweet.comment){
+				tweet.comment.forEach(function(data){
+					comment.push({text:data});
+				})
+				tweet.comment = comment;
+			}
+			var dateTime = new Date(tweet.createdAt)
+			tweet.createdAt =  dateTime.getMonth()  + "/" + dateTime.getDay() + "/" + dateTime.getFullYear();
+			tweets.push(tweet) 
+		})
 		$scope.stringlist = tweets;
 	}
 }]);
